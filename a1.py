28d868d1776a82f7099f351db7dcaa6d1385f53f
Sanskruti <sanskrutikandalkar@gmail.com>
2025-10-28 20:36:29 +0530

    first commit

---
+++ a1.py
@@ -0,0 +1,6 @@
+#1 print() function in python 
+rad= 13
+areac= 3.14*(rad*rad)
+print('Radius =', rad, 'Area of Circle =', areac)
+print(f'Radius = {rad}, Area of Circle = {areac}')
+print('Radius = ' + str(rad) + ' Area of Circle = ' + str(areac))
