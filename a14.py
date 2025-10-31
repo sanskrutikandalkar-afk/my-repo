abd945a397fa69ffb6bcb0540f76052a03901169
Sanskruti <sanskrutikandalkar@gmail.com>
2025-11-01 04:18:21 +0530

    twelveth commit

---
+++ a14.py
@@ -0,0 +1,9 @@
+# for as collection loop
+company = ['Tata','Infoys','Wipro','Mphasis']
+data = (34, 56,89,90,78,56)
+
+for c in company:
+    print(c, end=' ')
+print()
+for d in data:
+    print(d,end=' ')
