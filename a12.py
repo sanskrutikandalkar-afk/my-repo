02d1f03b7b2ab99501ae0b07be17d11b90781e7a
Sanskruti <sanskrutikandalkar@gmail.com>
2025-10-31 21:46:03 +0530

    tenth commit

---
+++ a12.py
@@ -0,0 +1,9 @@
+# for as collection loop 
+colors = ['green', 'red', 'yellow', 'pink','blue']
+data = (23,56,67,78,89,67)
+
+for c in colors:
+    print(c,end=' ')
+print()
+for d in data:
+    print(d,end=' ')
