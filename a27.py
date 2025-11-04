195f926879c8d28fcffa288f61e2fedfc73cbb75
Sanskruti <sanskrutikandalkar@gmail.com>
2025-11-04 22:11:05 +0530

    code

---
+++ a27.py
@@ -0,0 +1,7 @@
+# for loop as a numeric range loop
+for i in range (25):
+    print (i)
+print()
+for i in range (10):
+    print (i,end=' ')
+    
---
+++ a28.py
@@ -0,0 +1,4 @@
+# print only odd numbers
+for i in range(50):
+    if i % 2 == 1:
+        print(i, ends= ' ')
